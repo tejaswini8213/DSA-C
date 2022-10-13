44b0ff9dfdf57afd00ba9be1d52cdd484152f9bc
Tejaswini <tejaswinithota83@gmail.com~>
2022-10-13 11:31:09 +0530

    Addition of code
    Signed-off-by: Tejaswini <tejaswinithota83@gmail.com~>

--- double_ended.c
+++ double_ended.c
@@ -43,6 +43,7 @@ int dequeue_one(int queue[])
 {
     int size = sizeof(*queue / queue[0]);
     int val = queue[queue_one_front];
+    queue_one_front++;
     for (int i = 0; i < size; i++)
     {
         queue[queue_one_rear] = queue[queue_one_rear--];
@@ -57,22 +58,28 @@ int dequeue_two(int queue[])
     queue_two_front++;
     for (int i = 0; i < size; i++)
     {
-        queue[queue_two_rear] = queue[queue_one_rear--];
+        queue[queue_two_rear] = queue[queue_two_rear--];
     }
     return val;
 }
 
 void main()
 {
-    int array[5];
-    enqueue_one(array, 3);
-    enqueue_one(array, 4);
-    enqueue_one(array, 2);
-    enqueue_two(array, 1);
-    enqueue_two(array, 5);
-    for (int i = 0; i < 5; i++)
+    int n;
+    printf("Enter the array size");
+    scanf("%d", &n);
+    int array[n];
+    enqueue_one(array, 8);
+    enqueue_one(array, 7);
+    enqueue_one(array, 10);
+    enqueue_one(array, 9);
+    enqueue_two(array, 10);
+    enqueue_one(array, 5);
+    enqueue_two(array, 420);
+    for (int i = 0; i < n; i++)
     {
-        printf("%d ", array[i]);
+
+        printf("%d %d\n", dequeue_one(array), dequeue_two(array));
     }
     // printf("Dequeue value one %/d \n", val_one);
 }
